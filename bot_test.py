408f9e8d2b85131746537d3661c8382a2def958c
wert23239 <perfectkirby2339@gmail.com>
2018-09-03 16:03:24 -0700

    Add util test

--- bot_test.py
+++ bot_test.py
@@ -1,5 +1,19 @@
 import unittest
+import melee
+import util
+import logger
 class MyTest(unittest.TestCase):
    
-    def Utiltest(self):
-        Util()
+    def test_util(self):
+        utils=util.Util(logger=None)
+        expected=0
+        for i in range(len(utils.button_list)):
+            for j in range(len(utils.y_list)):
+                for k in range(len(utils.x_list)):
+                    result = (
+                        utils.convert_attack(utils.x_list[k],utils.y_list[j],utils.button_list[i]))
+                    self.assertEqual(result, expected)
+                    expected+=1    
+
+if __name__ == '__main__':
+    unittest.main()
